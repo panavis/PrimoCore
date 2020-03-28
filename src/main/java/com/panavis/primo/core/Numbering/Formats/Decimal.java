3a13d67b70894b028d152f725456befbcbd7f961
Anselme <mucunguzia@gmail.com>
2020-03-28 19:15:23 +0200

    Add first decimal numbers

--- src/main/java/com/panavis/primo/core/Numbering/Formats/Decimal.java
+++ src/main/java/com/panavis/primo/core/Numbering/Formats/Decimal.java
@@ -2,7 +2,22 @@ package com.panavis.primo.core.Numbering.Formats;
 
 import com.panavis.primo.core.Numbering.UnitNumbering;
 
+import java.util.Map;
+
 public class Decimal extends UnitNumbering {
+
+    public static final Map<String, Integer> FIRST_DECIMAL_NUMBERS = Map.ofEntries(
+            Map.entry("1", 1),
+            Map.entry("2", 2),
+            Map.entry("3", 3),
+            Map.entry("4", 4),
+            Map.entry("5", 5),
+            Map.entry("6", 6),
+            Map.entry("7", 7),
+            Map.entry("8", 8),
+            Map.entry("9", 9),
+            Map.entry("10", 10)
+    );
 
     public Decimal(String formatDisplay) {
         super(formatDisplay);
